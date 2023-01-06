506b2aa4f515fbd2a894e1dce05de1c9c4dbd960
Gitros <31899790+Gitros@users.noreply.github.com>
2023-01-06 20:34:56 +0100

    skrypt 1,2 php

--- organizer.php
+++ organizer.php
@@ -8,27 +8,59 @@
     <link rel="stylesheet" href="styl6.css">
 </head>
 <body>
+    <section class="the-whole-banner">
     <section class="banner">
-        <h2>Mój organizer</h2>
-    </section>
+            <h2>Mój organizer</h2>
+        </section>
 
-    <section class="banner">
-        <form action="" method="post">
-            <label for="podpis">Wpis wydarzenia: </label><input type="text" id="podpis" name="podpis">
-            <button name="zapisz">ZAPISZ</button>
-        </form>
-    </section>
+        <section class="banner">
+            <form action="" method="post">
+                <label for="podpis">Wpis wydarzenia: </label><input type="text" id="podpis" name="podpis">
+                <button name="zapisz">ZAPISZ</button>
+            </form>
 
-    <section id="banner-right-side">
-        <img src="logo2.png" alt="Mój organizer">
+                <?php 
+                $conn = mysqli_connect('localhost','root', '', 'egzamin6');
+
+                if(isset($_POST['podpis'])){
+                $podpis = $_POST['podpis'];
+                $query = "UPDATE `zadania` SET `wpis`= '$podpis' WHERE dataZadania = '2020-08-27'";
+                mysqli_query($conn, $query);
+                }
+
+                mysqli_close($conn);
+                ?> 
+
+
+        </section>
+
+        <section id="banner-right-side">
+            <img src="logo2.png" alt="Mój organizer">
+        </section>
     </section>
 
     <section id="main">
+        <?php 
+        $conn = mysqli_connect('localhost','root', '', 'egzamin6');
 
+        $query2= mysqli_query($conn,'SELECT dataZadania, miesiac, wpis FROM `zadania` WHERE miesiac LIKE "lipiec"');
+
+        while($row2 = mysqli_fetch_assoc($query2)){
+            echo "<section class='main'>$row2[dataZadania] $row2[miesiac] $row2[wpis]</section>";
+        };
+        
+        
+
+        mysqli_close($conn);
+        ?>
     </section>
 
     <section id="footer">
+    <?php 
+        $conn = mysqli_connect('localhost','root', '', 'egzamin6');
 
+        mysqli_close($conn);
+    ?>
         <p>Stronę wykonał: kozak</p>
     </section>
 </body>
