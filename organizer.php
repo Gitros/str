704e9910139cf0064c9a58ea25b010bca168ceb6
Gitros <31899790+Gitros@users.noreply.github.com>
2023-01-06 19:41:35 +0100

    strona1

---
+++ organizer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organizer</title>
+    <link rel="stylesheet" href="styl6.css">
+</head>
+<body>
+    <section class="banner">
+        <h2>Mój organizer</h2>
+    </section>
+
+    <section class="banner">
+        <form action="" method="post">
+            <label for="podpis">Wpis wydarzenia: </label><input type="text" id="podpis" name="podpis">
+            <button name="zapisz">ZAPISZ</button>
+        </form>
+    </section>
+
+    <section id="banner-right-side">
+        <img src="logo2.png" alt="Mój organizer">
+    </section>
+
+    <section id="main">
+
+    </section>
+
+    <section id="footer">
+
+        <p>Stronę wykonał: kozak</p>
+    </section>
+</body>
+</html>
